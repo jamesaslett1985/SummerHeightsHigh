997bdd72af2b429fdf881f01dccf930a70e2b255
jamesaslett1985 <jamesaslett1985@gmail.com>
2020-12-22 16:52:20 +0000

    Latest

---
+++ Website/Controllers/CharactersController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using Website.Models;
+
+namespace Website.Controllers
+{
+    public class CharactersController : Controller
+    {
+        public IActionResult Index()
+        {
+            Characters characters = new Characters()
+            {
+                Name = "Name from HomeController",
+                Age = 18,
+                MainCharacteristic = "MainCharacteristic from HomeController",
+                Quote = "Quote from HomeController"
+               
+            };
+            return View(characters);
+        }
+    }
+}
---
+++ Website/Models/Characters.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Website.Models
+{
+    public class Characters
+    {
+        public string Name { get; set; }
+
+        public int Age{ get; set; }
+
+        public string MainCharacteristic { get; set; }
+
+        public string Quote { get; set; }
+    }
+}
